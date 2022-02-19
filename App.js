940061cccc975499b2ba2b6064a5705e9b61e41d
Sara583 <77129891+Sara583@users.noreply.github.com>
2022-02-19 13:49:32 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Linking, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const[name,setName]=useState('No Clicks happened')
+  const[number,setNumber]=useState(0)
+  
+
+  const click=()=>{
+
+    setName("Your Counter =")
+    setNumber(number+5)
+
+      
+  }
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    
+    <Text style={styles.textOne}>Count by 5</Text>
+      
+      <Text style={styles.textTwo}>{name} {number}</Text>
+      <Button   title='ADD 5' onPress={click}></Button>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width:'auto',
+    height:'auto',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF1CE',
     alignItems: 'center',
     justifyContent: 'center',
+  borderBottomColor:"green",
   },
+  textOne:{
+    
+    color:"grey",
+    fontSize:70
+  },
+  textTwo:{
+    margin:20,
+    color:"black"
+  },
+  
+
 });
